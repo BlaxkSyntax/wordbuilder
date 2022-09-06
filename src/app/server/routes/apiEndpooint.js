36a0edd01ee0847fabe079efe1307d9759a5b75a
blaxk.syntax <blaxk.syntax@gmail.com>
2022-09-06 14:47:00 +0200

    fixing protocol

--- src/app/server/routes/apiEndpooint.js
+++ src/app/server/routes/apiEndpooint.js
@@ -9,8 +9,8 @@ class ServerRoutes {
 
     code(server){
         server.post('/code', function (req, res) {
-            console.log(req.body);
-            res.json(codeToBreak.break(Array.from(String(req.body.code), Number)));
+            console.log(req.body.code);
+            res.json(codeToBreak.break(Array.from(Stringreq.body.code, Number)));
         })
     }
 }
